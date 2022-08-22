89580bf13b275b060e0cfccedc93516edc2cbc4a
Mic <michelelt660@gmail.com>
2022-08-23 00:28:57 +0200

    renamed hello components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class HelloWorld extends React.Component {
+class Hello extends React.Component {
     render(){
         return <h1 className="text-3xl font-bold flex justify-center">Hello, World!</h1>
     }
 }
-export {HelloWorld};
+export {Hello};
 
