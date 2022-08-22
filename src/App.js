31af5f13e2ccb3e9f08ccf9d967a033aae48177b
Mic <michelelt660@gmail.com>
2022-08-22 23:22:22 +0200

    components-02 ex

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
+import React from 'react';
 
+class HelloWorld extends React.Component {
+    render(){
+        return <h1 className="text-3xl font-bold flex justify-center">Hello, World!</h1>
+    }
+}
+export {HelloWorld};
