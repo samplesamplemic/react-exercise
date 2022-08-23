73703852bc1a47abffc047b1991b0dc74f3ab976
Mic <michelelt660@gmail.com>
2022-08-23 11:37:37 +0200

    components-06 ex

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export class Hello extends React.Component {
-    render(){
-        return <h1 className="text-3xl font-bold flex justify-center">Hello, World!</h1>
-    }
+  render() {
+    return (
+        <>
+        <h1 className="text-3xl font-bold">Hello, World!</h1>
+        <p className="text-xl">What a beautiful day</p>
+        </>
+    );
+      
+  }
 }
 //export {Hello};
 
 const App = () => {
-    return ( 
-        <div>
-            <Hello />
-        </div>
-     );
-}
- 
-export default App ;
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
