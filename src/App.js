07da72eac16b3fb1f22c22efa4fe6122ec101718
Mic <michelelt660@gmail.com>
2022-08-23 09:54:35 +0200

    components-03 ex

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-class Hello extends React.Component {
+export class Hello extends React.Component {
     render(){
         return <h1 className="text-3xl font-bold flex justify-center">Hello, World!</h1>
     }
 }
-export {Hello};
+//export {Hello};
 
+const App = () => {
+    return ( 
+        <div>
+            <Hello />
+        </div>
+     );
+}
+ 
+export default App ;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import  ReactDOM from 'react-dom/client';
-import {Hello} from './App.js'
+import App from './App.js'
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Hello />
+    <App />
 );
