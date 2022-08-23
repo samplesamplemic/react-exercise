7b6d43e99df7a46accc58c70155dd782a78440f6
Mic <michelelt660@gmail.com>
2022-08-23 17:14:08 +0200

    props-03 ex

--- src/props/props.js
+++ src/props/props.js
@@ -1,11 +1,16 @@
 const Welcome = (props) => {
     return (
-        <p className="text-l flex items-center">Welcome, {props.name}!</p>
+        <>
+             <p className="text-l flex items-center">Welcome, {props.name}!</p>
+             <p className="text-l flex items-center">Your age is {props.age}</p>
+        </>
+       
     )
 }
  
 Welcome.defaultProps = {
-    name: "everyone"
+    name: "everyone",
+    age:25
 };
 
 export default Welcome;
