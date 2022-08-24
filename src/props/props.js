76646a6409ffe11048bdb232c4f8a878d917242d
Mic <michelelt660@gmail.com>
2022-08-24 10:05:12 +0200

    conditional-rendering-02 ex

--- src/props/props.js
+++ src/props/props.js
@@ -2,7 +2,8 @@ export const Welcome = (props) => {
     return (
         <>
             <p className="text-l flex items-center">Welcome, {props.name}!</p>
-             {props.age >= 18 && <Age age = {props.age} />} {/*conditional rendering */}
+             {/* {(props.age >= 18) && <Age age = {props.age} />} conditional rendering 01 */}
+             {(props.age) && <Age age = {props.age} />} {/*conditional rendering 02*/}
         </>
        
     )
