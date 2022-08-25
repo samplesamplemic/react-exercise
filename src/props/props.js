c3175f0dd3b476cbf21e0be7e1df7fc338095a14
Mic <michelelt660@gmail.com>
2022-08-25 14:26:06 +0200

    added classes

--- src/props/props.js
+++ src/props/props.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
 //class
 export class Welcome extends React.Component {
-    render(){
-        return (
-            <>
-                <p className="text-l flex items-center">Welcome, {this.props.name}!</p>
-            <Age age={this.props.age} />
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <p className="text-l flex items-center">Welcome, {this.props.name}!</p>
+        <Age age={this.props.age} />
+        {/* {(props.age >= 18) && <Age age = {props.age} />} conditional rendering 01 */}
+        {/* {(props.age) && <Age age = {props.age} />} conditional rendering 02*/}
+        {/* {(props.age > 18 && props.age < 65) && <Age age = {props.age} />} conditional rendering 03*/}
+        {/* {(props.age > 18 && props.age < 65) && props.name == "John" && <Age age = {props.age} />} conditional rendering 04*/}
+      </>
+    );
+  }
 }
 
 //function
@@ -23,29 +27,25 @@ export class Welcome extends React.Component {
 //              {/* {(props.age > 18 && props.age < 65) && <Age age = {props.age} />} conditional rendering 03*/}
 //              {/* {(props.age > 18 && props.age < 65) && props.name == "John" && <Age age = {props.age} />} conditional rendering 04*/}
 //         </>
-       
+
 //     )
 // }
- 
+
 Welcome.defaultProps = {
-    name: <strong>SamSample</strong>,
+  name: <strong>SamSample</strong>,
 };
 
-
-
 export const Age = (props) => {
-    return ( 
-        <>
-            {   /*conditional rendering 05*/
-                props.age > 18 
-                ?<p className="text-l flex items-center">Your age is {props.age}</p>
-                :<p className="text-l flex items-center">You are very young!</p>
-            }
-            
-       
-        </>
-     );
-}
- 
-
-
+  return (
+    <>
+      {
+        /*conditional rendering 05*/
+        props.age > 18 ? (
+          <p className="text-l flex items-center">Your age is {props.age}</p>
+        ) : (
+          <p className="text-l flex items-center">You are very young!</p>
+        )
+      }
+    </>
+  );
+};
