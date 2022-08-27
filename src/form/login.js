ea0a038a2a11cafc1773ea782b269d3cda9d540c
Mic <michelelt660@gmail.com>
2022-08-27 12:44:08 +0200

    form-04 ex

--- src/form/login.js
+++ src/form/login.js
@@ -15,6 +15,15 @@ const Login = (props) => {
 //     setLogin((prev) => ({ ...prev, [e.target.name]: e.target.checked}))
 //  }
 
+//form-04 ex
+const handleReset = () => {
+  setLogin({
+    username: '',
+    password: '',
+    remember: false
+  })
+}
+
  console.log(login);
   return (
     <>
@@ -22,6 +31,7 @@ const Login = (props) => {
         <input
           type="text"
           name="username"  //must be the same of one object key into useState Hook or it will created a new one {key: value} object
+          value={login.username}
           onChange={handleChange}
           placeholder="name"
           className="bg-slate-200"
@@ -29,6 +39,7 @@ const Login = (props) => {
         <input
           type="password"
           name="password"
+          value={login.password}
           onChange={handleChange}
           placeholder="password"
           className="bg-slate-200"
@@ -37,19 +48,29 @@ const Login = (props) => {
           <input
             type="checkbox"
             name="remember"
+            value={login.remember}
             onChange={handleChange}
             checked={login.remember}
             className="bg-slate-200 fit-content mr-2"
           />
           Remember me
         </div>
+        <div className= "flex gap-2 justify-around">
         <button //form-03 ex
-        className="bg-slate400 border border-black rounded-md" 
+        className="bg-slate-200 border border-black rounded-md p-1" 
         disabled={login.username === '' && login.password === '' ? true : false}
         onClick={props.onLogin} //in App
         >
           Login
         </button>
+        <button
+        className="bg-slate-200 border border-black rounded-md p-1"
+        onClick={handleReset}
+        >
+         Reset
+        </button>
+        </div>
+       
       </div>
     </>
   );
