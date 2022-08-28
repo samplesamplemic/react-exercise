93d7088f59999c2e070fb40c538ed1e5fb653d34
Mic <michelelt660@gmail.com>
2022-08-28 11:17:49 +0200

    lists-03 ex

--- src/list/todoList.js
+++ src/list/todoList.js
@@ -1,41 +1,46 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 const TodoList = () => {
-    const items = [
-        'a', 'b', 'c', 'd', 'e', 'f'
-    ]
-    const [todo, setTodo] = useState('')
-   const handleChange = (e) => {
-   setTodo(e.target.value)
-   }
-   const handleSubmit = (e) => {
-    e.preventDefault();
-    items.push(todo)
-    setTodo('')
-    console.log(items);
-   }
-
-    return ( 
-        <>
-        <div>
-        <p className="text-xl font-semibold pt-4 mb-4 border-t-2 flex justify-center">Todo</p>
-        <input type="text" placeholder="Todo" className='bg-slate-200 rounded-md p-2'
-        onChange={handleChange}
-        value={todo}
-        />
-        <button className="bg-slate-200 rounded-md ml-4 p-2"
-        type="submit"
-        onClick={handleSubmit}
-        >INS</button>
-
-        </div>
-        <ul>
-        {items.map((item, index) => 
-            <li key={index + item}>{item}</li>
-        )}
-        </ul>
-        </>
-     );
-}
+  const [todo, setTodo] = useState(''); //i called todo instead of items
  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo((prev) => [...prev, document.querySelector('#myId').value])
+    console.log(todo); //lists-03 ex
+    console.log(document.querySelector('#myId').value); //lists-03 ex
+  };
+
+  
+  return (
+    <>
+      <div>
+        <p className="text-xl font-semibold pt-4 mb-4 border-t-2 flex justify-center">
+          Todo
+        </p>
+        <input
+          type="text"
+          placeholder="Todo"
+          id='myId'
+          className="bg-slate-200 rounded-md p-2"
+          name='mytodo'
+        
+        />
+        <button
+          className="bg-slate-200 rounded-md ml-4 p-2"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          INS
+        </button>
+      </div>
+      <ul>
+        {Array.isArray(todo) ? todo.map((item, index) => (
+          <li key={index + item}>{item}</li>
+        )) : null}
+       
+      </ul>
+    </>
+  );
+};
+
 export default TodoList;
