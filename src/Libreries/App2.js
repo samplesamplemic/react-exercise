c3870cd902454303d64ed3d41d2520c0288cdb52
Mic <michelelt660@gmail.com>
2022-09-09 09:13:22 +0200

    react-router-04 ex

--- src/Libreries/App2.js
+++ src/Libreries/App2.js
@@ -1,23 +1,34 @@
-import { useEffect, useState } from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
-import { Welcome } from '../props/props';
-import Counter from '../state/count';
-import ShowGithubUser from './ShowGithubUser';
+import { useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Welcome } from "../props/props";
+import Counter from "../state/count";
+import ShowGithubUser from "./ShowGithubUser";
 
 const App2 = () => {
-    let user = 'samplesamplemic';
-    //const [user, setUser] = useState('');setUser((e) => e.target.value)
-    return (
-        <div className="flex text-center flex-col">
-        <h1 className='text-xl font-bold mt-3 py-4 border-t-2 border-black'> React Router </h1>
-        <Routes>
-            <Route  path ='/' element={<Welcome name='mic2' />}  />
-            <Route path='/counter' element={<Counter />} />
-            <Route path={`/users/:${user}`} element={<ShowGithubUser username={user} />} /> 
-         </Routes>
+  let user = "samplesamplemic";
+  //const [user, setUser] = useState('');setUser((e) => e.target.value)
+  return (
+    <div className="flex text-center flex-col">
+      <h1 className="text-xl font-bold mt-3 py-4 border-t-2 border-black">
+        {" "}
+        React Router{" "}
+      </h1>
+      <Routes>
+        <Route path="/" element={<Welcome name="mic2" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route
+          path={`/users/:${user}`}
+          element={<ShowGithubUser username={user} />}
+        />
+      </Routes>
+      <div className=" flex flex-wrap justify-center gap-4 mt-4 border-t-2 text-lg font-semibold text-blue-600  ">
+      <h2 className="basis-full text-black">Links</h2>
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to={`/users/:${user}`}>Github User</Link>
+      </div>
+    </div>
+  );
+};
 
-        </div> 
-     );
-}
- 
 export default App2;
